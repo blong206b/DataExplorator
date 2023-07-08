f8780116d89d0f3de1e428d90919d031eb454b3f
blong206b <blong206b@gmail.com>
2023-07-08 12:26:57 -0400

    commit all

---
+++ js practice/index.js
@@ -0,0 +1,32 @@
+// var name = "Peter"
+var number = 42
+var number2 = 41
+console.log(number === number2)
+
+var animals = ["rabbit", "snake", "pig"]
+console.log(animals)
+console.log(animals.length)
+console.log(animals[1])
+
+// object
+var user = {
+    name: "Peter",
+    age: 42,
+    isLoggedIn: false
+}
+
+user.isLoggedIn = true
+
+console.log(user)
+
+
+
+
+
+
+
+let numOne = 4
+let numTwo = 3
+
+let sum = 4 + 3
+console.log(sum)
